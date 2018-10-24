8dd69ce5bea45a3c8f0d9f77c019c16e65626d03
pddys <c3xh5i@gmail.com>
2018-10-24 20:52:15 +0100

    REMOVE: capitals

--- js/pickRandom.js
+++ js/pickRandom.js
@@ -154,62 +154,62 @@ const dataAmRep = [
 	{
 		title: "Cirque of the Dead",
 		desc: "Join us this Halloween season for Cirque of the Dead, a naughty nightmare of circus spectacle!",
-		date: "OCT 19 - OCT 31",
+		date: "Oct 19 - Oct 31",
 	},
 	{
 		title: "Danse Macabre: A Walking Dead Burlesque",
 		desc: "This… This is Lucille, and she is awesome. Who want’s the honor?",
-		date: "OCT 21 8PM",
+		date: "Oct 21 8PM",
 	},
 	{
 		title: "Boston Percussive Arts Festival: Trio Mokili feat. Makaya McCraven",
 		desc: "Boston Percussive Arts Festival brings rhythm to our city and welcomes Trio Mokili and the Boston Round Robin Rhythm Session.",
-		date: "OCT 24 8PM",
+		date: "Oct 24 8PM",
 	},
 	{
 		title: "The Haunt",
 		desc: "The story of Rapunzel as a Halloween musical theater cabaret and costumed dance party.",
-		date: "OCT 28 7PM",
+		date: "Oct 28 7PM",
 	},
 	{
 		title: "Rock House Comedy",
 		desc: "Get ready to rock and laugh!",
-		date: "NOV 1 8PM",
+		date: "Nov 1 8PM",
 	},
 	{
 		title: "Billy Dean Thomas",
 		desc: 'Also known as “The Queer B.I.G.”, Billy Dean is a musician who challenges the hip hop game with lyrics that align with #BlackLivesMatter and intersectional feminism.',
-		date: "NOV 2 10PM",
+		date: "Nov 2 10PM",
 	},
 	{
 		title: "Black Light",
 		desc: "Jomama Jones performs a spiritual revival for turbulent times, drawing from Prince, Sade, Diana Ross, and Tina Turner.",
-		date: "MAY 15 - 19, 2019",
+		date: "May 15 - 19, 2019",
 	},
 	{
 		title: "Believe it or Naughty: A Burlesque of Lore and Legend",
 		desc: "A Burlesque of Lore and Legend.",
-		date: "NOV 8 & NOV 11",
+		date: "Nov 8 & Nov 11",
 	},
 	{
 		title: "Billy Wylder: Strike the Match",
 		desc: "GStrike the Match is the dynamic record release concert experience featuring Boston’s own art-folk-rock band, Billy Wylder.",
-		date: "NOV 9 8:30PM",
+		date: "Nov 9 8:30PM",
 	},
 	{
 		title: "Barber Shop Chronicles",
 		desc: "Newsroom and political platform, confession box and stadium—for generations, barber shops have been places where African men gather to discuss the world.",
-		date: "DEC 5 - JAN 5, 2019",
+		date: "Dec 5 - Jan 5, 2019",
 	},
 	{
 		title: "We Live in Cairo",
 		desc: "This new musical follows six revolutionary students armed with laptops and cameras, guitars and spray cans as they come of age in a contemporary Cairo.",
-		date: "MAY 14 - JUN 16, 2019",
+		date: "May 14 - Jun 16, 2019",
 	},
 	{
 		title: "Sven Ratzke",
 		desc: "Featuring songs by Brecht, Bowie, Weill, and Wainwright, prepare to meet pimps, lovers, angels, and devils in this intimate cabaret performance.",
-		date: "APR 18, 2019 8PM",
+		date: "Apr 18, 2019 8pm",
 	},
 ]
 
